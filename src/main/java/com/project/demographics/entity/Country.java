17bc425f6d4c84e79722c97f2c03ca6ec6fa0b46
BJ <balazzy@gmail.com>
2024-12-19 16:29:37 +0530

    Commit after adding service file and modifying the Country entity, adding getter setters manually, because lombok doesn't work on eclipse.

--- src/main/java/com/project/demographics/entity/Country.java
+++ src/main/java/com/project/demographics/entity/Country.java
@@ -1,11 +1,11 @@
 package com.project.demographics.entity;
 
 import jakarta.persistence.*;
-import lombok.Data;
+
 
 import java.util.List;
 
-@Data
+
 @Entity
 @Table(name = "country")
 public class Country {
@@ -51,4 +51,142 @@ public class Country {
 
     @Transient
     private List<CountryLanguage> countryLanguages;
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getContinent() {
+		return continent;
+	}
+
+	public void setContinent(String continent) {
+		this.continent = continent;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
+	}
+
+	public float getSurfaceArea() {
+		return surfaceArea;
+	}
+
+	public void setSurfaceArea(float surfaceArea) {
+		this.surfaceArea = surfaceArea;
+	}
+
+	public Integer getIndepYear() {
+		return indepYear;
+	}
+
+	public void setIndepYear(Integer indepYear) {
+		this.indepYear = indepYear;
+	}
+
+	public int getPopulation() {
+		return population;
+	}
+
+	public void setPopulation(int population) {
+		this.population = population;
+	}
+
+	public Float getLifeExpectancy() {
+		return lifeExpectancy;
+	}
+
+	public void setLifeExpectancy(Float lifeExpectancy) {
+		this.lifeExpectancy = lifeExpectancy;
+	}
+
+	public Float getGnp() {
+		return gnp;
+	}
+
+	public void setGnp(Float gnp) {
+		this.gnp = gnp;
+	}
+
+	public Float getGnpOld() {
+		return gnpOld;
+	}
+
+	public void setGnpOld(Float gnpOld) {
+		this.gnpOld = gnpOld;
+	}
+
+	public String getLocalName() {
+		return localName;
+	}
+
+	public void setLocalName(String localName) {
+		this.localName = localName;
+	}
+
+	public String getGovernmentForm() {
+		return governmentForm;
+	}
+
+	public void setGovernmentForm(String governmentForm) {
+		this.governmentForm = governmentForm;
+	}
+
+	public String getHeadOfState() {
+		return headOfState;
+	}
+
+	public void setHeadOfState(String headOfState) {
+		this.headOfState = headOfState;
+	}
+
+	public Integer getCapital() {
+		return capital;
+	}
+
+	public void setCapital(Integer capital) {
+		this.capital = capital;
+	}
+
+	public String getCode2() {
+		return code2;
+	}
+
+	public void setCode2(String code2) {
+		this.code2 = code2;
+	}
+
+	public List<City> getCities() {
+		return cities;
+	}
+
+	public void setCities(List<City> cities) {
+		this.cities = cities;
+	}
+
+	public List<CountryLanguage> getCountryLanguages() {
+		return countryLanguages;
+	}
+
+	public void setCountryLanguages(List<CountryLanguage> countryLanguages) {
+		this.countryLanguages = countryLanguages;
+	}
+    
+    
 }
--- src/main/java/com/project/demographics/service/CountryService.java
+++ src/main/java/com/project/demographics/service/CountryService.java
@@ -1,5 +1,36 @@
 package com.project.demographics.service;
 
-public class CountryService {
+import com.project.demographics.entity.City;
+import com.project.demographics.entity.Country;
+import com.project.demographics.entity.CountryLanguage;
+import com.project.demographics.repository.CityRepository;
+import com.project.demographics.repository.CountryLanguageRepository;
+import com.project.demographics.repository.CountryRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+public class CountryService {
+    private final CountryRepository countryRepository;
+    private final CityRepository cityRepository;
+    private final CountryLanguageRepository countryLanguageRepository;
+
+    public CountryService(CountryRepository countryRepository, CityRepository cityRepository, CountryLanguageRepository countryLanguageRepository) {
+        this.countryRepository = countryRepository;
+        this.cityRepository = cityRepository;
+        this.countryLanguageRepository = countryLanguageRepository;
+    }
+
+    public List<Country> getAllCountriesWithDetails() {
+        List<Country> countries = countryRepository.findAll();
+        for (Country country : countries) {
+            List<City> cities = cityRepository.findByCountryCode(country.getCode());
+            country.setCities(cities);
+
+            List<CountryLanguage> languages = countryLanguageRepository.findByIdCountryCode(country.getCode());
+            country.setCountryLanguages(languages);
+        }
+        return countries;
+    }
 }
