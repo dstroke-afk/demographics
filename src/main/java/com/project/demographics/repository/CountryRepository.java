307903d3b57dc21e7bf3dbd0164611962e9f25cc
BJ <balazzy@gmail.com>
2024-12-19 17:12:59 +0530

    CountryRepository.java: Added a method to find a country by its name. This commit works. It fetches a country's details and the city and country language values in arrays by country's name provided as a path variable.

--- src/main/java/com/project/demographics/repository/CountryRepository.java
+++ src/main/java/com/project/demographics/repository/CountryRepository.java
@@ -3,5 +3,8 @@ package com.project.demographics.repository;
 import com.project.demographics.entity.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CountryRepository extends JpaRepository<Country, String> {
+    Optional<Country> findByName(String name);
 }
