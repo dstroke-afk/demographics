1fff7b63d5a6b65121ac22e41c8520569ef48ab4
BJ <balazzy@gmail.com>
2024-12-19 16:34:30 +0530

    Added controller for the api.

--- src/main/java/com/project/demographics/controller/CountryController.java
+++ src/main/java/com/project/demographics/controller/CountryController.java
@@ -1,5 +1,22 @@
 package com.project.demographics.controller;
 
-public class CountryController {
+import com.project.demographics.entity.Country;
+import com.project.demographics.service.CountryService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+public class CountryController {
+    private final CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @GetMapping("/countries")
+    public List<Country> getAllCountries() {
+        return countryService.getAllCountriesWithDetails();
+    }
 }
