278237448b517dc9e15b20964d3aba7242e5c50d
BJ <balazzy@gmail.com>
2024-12-19 17:06:52 +0530

    CountryController.java: Added a new endpoint for /countries/{name}

--- src/main/java/com/project/demographics/controller/CountryController.java
+++ src/main/java/com/project/demographics/controller/CountryController.java
@@ -2,7 +2,9 @@ package com.project.demographics.controller;
 
 import com.project.demographics.entity.Country;
 import com.project.demographics.service.CountryService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -18,5 +20,12 @@ public class CountryController {
     @GetMapping("/countries")
     public List<Country> getAllCountries() {
         return countryService.getAllCountriesWithDetails();
+    }
+
+    @GetMapping("/countries/{name}")
+    public ResponseEntity<Country> getCountryByName(@PathVariable String name) {
+        return countryService.getCountryWithDetailsByName(name)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
